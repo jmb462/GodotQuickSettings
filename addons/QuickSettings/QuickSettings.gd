f700af9bd0f207203a786ed23e38da2a5e8a628f
Jean-Michel Bernard <jmb462@gmail.com>
2022-01-30 10:58:48 +0100

    Clean at exit

--- addons/QuickSettings/QuickSettings.gd
+++ addons/QuickSettings/QuickSettings.gd
@@ -6,3 +6,7 @@ var dock : Control
 func _enter_tree():
 	dock = preload("res://addons/QuickSettings/QuickSettings.tscn").instance()
 	add_control_to_dock(DOCK_SLOT_RIGHT_UL, dock)
+
+func _exit_tree() -> void:
+	remove_control_from_docks(dock)
+	dock.queue_free()
